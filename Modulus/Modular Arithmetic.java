b8a7d64cca28ddd6024d2d37d15bd545e0fb1580
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2024-12-27 23:05:55 +0530

    Create Modular Arithmetic.java

---
+++ Modulus/Modular Arithmetic.java
@@ -0,0 +1,12 @@
+public static int modExp(int base, int exp, int mod) {
+    int result = 1;
+    base = base % mod;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+        exp = exp >> 1;
+        base = (base * base) % mod;
+    }
+    return result;
+}
